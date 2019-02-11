2bcb152eca8c321339e35086631f3e5aff7849d0
michalkandybowicz <eejdzent@gmail.com>
2019-02-11 15:03:55 +0100

    dodana funkcja error msg Signed-off-by: michalkandybowicz <eejdzent@gmail.com>
    Signed-off-by: michalkandybowicz <eejdzent@gmail.com>

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -12,11 +12,9 @@ Data table structure:
 
 # everything you'll need is imported:
 # User interface module
-import ui
+
 # data manager module
-import data_manager
 # common module
-import common
 
 
 def start_module():
