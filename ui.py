eeb3ad4e976a3e76b22f7e434952006a783d475b
Anita Mowczan <anita.mowczan@gmail.com>
2019-02-12 10:19:57 +0100

    I added a table. Please check if it's correct

--- ui.py
+++ ui.py
@@ -1,28 +1,90 @@
 """ User Interface (UI) module """
 
 
-def print_table(table, title_list):
-    """
-    Prints table with data.
+# -------------------------------- TABLE FUNCTIONS-------------	
+	max_length_column = []
+	
 
-    Example:
-        /-----------------------------------\
-        |   id   |      title     |  type   |
-        |--------|----------------|---------|
-        |   0    | Counter strike |    fps  |
-        |--------|----------------|---------|
-        |   1    |       fo       |    fps  |
-        \-----------------------------------/
+	# define max length of colums in table
+	def max_length(table, title_list):
+	    max_length_column = []
+	    for column in range(len(title_list)):
+	        temp = 0
+	        for row in range(len(table)):
+	            if len(str(table[row][column])) > temp:
+	                temp = len(str(table[row][column]))
+	                temp = int(temp)
+	        max_length_column.append(temp)
+	    return max_length_column
+	
+	# define added length of table based on column length
+	def sum_length_table(max_length_column, title_list):
+	    sum_length = len(title_list)*2
+	    for i in range(len(max_length_column)):
+	        sum_length += max_length_column[i]
+	    return sum_length
+	
+	# prints upper part of table
+	def print_top_border(sum_length):
+	    print('/', ('-'*(sum_length-3)), '\\')
+	
 
-    Args:
-        table (list): list of lists - table to display
-        title_list (list): list containing table headers
+	# prints middle part of table
+	def print_middle_border(sum_length):
+	    print('|', ('-'*(sum_length-3)), '|')
+	
 
-    Returns:
-        None: This function doesn't return anything it only prints to console.
-    """
+	# prints bottom part of table
+	def print_bottom_border(sum_length):
+	    print('\\', ('-'*(sum_length-3)), '/')
+	
 
-    # your goes code
+	# prints columns title - each module has diffrent title_list
+	def print_columns_title(title_list, max_length_column):
+	    for col_i in range(len(title_list)):
+	        col = title_list[col_i]
+	        width = max_length_column[col_i]
+	        print('|', col.center(width),  end='')
+	    print('|')
+	
+
+	# prints what is in table - table is different for each module
+	def print_items_table(table, max_length_column, sum_length, title_list):
+	    row_number = 1
+	    for row in table:
+	        print_middle_border(sum_length)
+	        for col_i in range(len(row)):
+	            col = row[col_i]
+	            width = max_length_column[col_i]
+	            if col_i == 0:
+	                print('|', str(row_number).center(width),  end='')
+	            else:
+	                print('|', col.center(width),  end='')
+	        row_number += 1
+	        print('|')
+	
+
+	# print table based on tilte list and table from modules
+	def print_table(table, title_list):
+	    """
+	    Prints table with data.
+	    Args:
+	        table (list): list of lists - table to display
+	        title_list (list): list containing table headers
+	    Returns:
+	        None: This function doesn't return anything it only prints to console.
+	    """
+	
+	    max_length_column = []
+	    temp_table = table.copy()
+	    temp_table.append(title_list)
+	    max_length_column = max_length(temp_table, title_list)
+	    sum_length = sum_length_table(max_length_column, title_list)
+	    print_top_border(sum_length)
+	    print_columns_title(title_list, max_length_column)
+	    print_items_table(table, max_length_column, sum_length, title_list)
+	    print_bottom_border(sum_length)
+
 
 
 def print_result(result, label):
